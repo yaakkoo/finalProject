e0e4f783fd51e4fb2740a226fb2608b1dbc09a32
yazan abbas <ayazan630@gmail.com>
2021-08-26 10:31:51 +0300

    heroku

--- controller/user.js
+++ controller/user.js
@@ -5,7 +5,7 @@ const nodemailer = require('nodemailer')
 const bcryptjs = require('bcryptjs');
 const cloudinary = require('cloudinary')
 
-let client = redis.createClient()
+let client = redis.createClient(3001)
 
 
 let transporter = nodemailer.createTransport({
