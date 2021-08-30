81caed655149988744058dbb517445e04b3cf6b4
yazan abbas <ayazan630@gmail.com>
2021-08-30 07:03:24 +0300

    'redis'

--- router/fight.js
+++ router/fight.js
@@ -6,9 +6,9 @@ require('express-async-error')
 const router = express.Router()
 
 router.post('/randomMatch', Auth, randomMatch)
-router.post('/friendMatch', friendMatch)
+router.post('/friendMatch', Auth, friendMatch)
 router.post('/decline', Auth, decline)
-router.post('/accept',  accept)
+router.post('/accept', Auth, accept)
 router.post('/endFight', Auth, endFight)
 router.post('/getMatchInfo',Auth , getMatchInfo)
 module.exports = router
